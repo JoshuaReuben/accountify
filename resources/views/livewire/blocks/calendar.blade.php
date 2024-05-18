8c6461c3fe45ef72092b4d5e48a1039c7c442f89
JoshuaReuben <joshuareubenc.v1@gmail.com>
2024-05-19 01:41:47 +0800

    calendar optimize

--- resources/views/livewire/blocks/calendar.blade.php
+++ resources/views/livewire/blocks/calendar.blade.php
@@ -9,6 +9,7 @@ new class extends Component {
     public $firstDayOfMonth;
 
     public $daysInWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    public $months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; // Full name of the months
 
     public function mount()
     {
@@ -55,40 +56,57 @@ new class extends Component {
 <div>
 
     <div>
-        <h1 class="text-center">Calendar</h1>
-        <div class="text-center">Month: {{ $month }} Year: {{ $year }} </div>
-        <button wire:click="prevMonth" class="p-4 m-4 border">Prev</button>
-        <button wire:click="nextMonth" class="p-4 m-4 border">Next</button>
-        <button wire:click="today" class="p-4 m-4 border">Today</button>
+        <h1 class="text-center">CALENDAR</h1>
+        <div class="text-center"> {{ $months[$month - 1] }} {{ $year }} </div>
+        <button wire:click="prevMonth" type="button"
+            class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+            Prev
+        </button>
+        <button wire:click="nextMonth" type="button"
+            class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+            Next
+        </button>
+        <button wire:click="today" type="button"
+            class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+            Today
+        </button>
+
 
 
         <br><!-- Display the calendar days -->
-        {{-- Header Week Days --}}
-        <div class="grid grid-cols-7 text-center">
-            @foreach ($daysInWeek as $day)
-                <div class="py-1 border border-gray-600 md:p-4 dark:border-white">{{ $day }}
-                </div>
-            @endforeach
-        </div>
-        <div wire:loading wire:target="prevMonth, nextMonth, today"
-            class="w-full h-full py-1 text-center border border-gray-600 md:p-4 dark:border-white">
-            Loading ...
+        <div class="bg-gray-50 dark:bg-gray-800 ">
+            {{-- Header Week Days --}}
+            <div class="grid grid-cols-7 text-center">
+                @foreach ($daysInWeek as $day)
+                    <div class="py-1 text-xs border border-gray-600 md:text-md lg:text-lg md:p-4 dark:border-gray-500">
+                        {{ $day }}
+                    </div>
+                @endforeach
+            </div>
+
+            <div wire:loading wire:target="prevMonth, nextMonth, today"
+                class="w-full h-full py-1 text-center border border-gray-600 md:p-4 dark:border-gray-500">
+                Loading ...
+            </div>
+
+            <div class="relative grid grid-cols-7 text-center">
+                {{-- Trailing Filler Box  --}}
+                @for ($i = 0; $i < $firstDayOfMonth; $i++)
+                    <div wire:loading.remove wire:target="prevMonth, nextMonth, today"
+                        class='py-1 border border-gray-600 md:p-4 dark:border-gray-500'></div>
+                @endfor
+
+                @for ($day = 1; $day <= $daysInMonth; $day++)
+                    <div wire:loading.remove wire:target="prevMonth, nextMonth, today"
+                        class='py-1 border border-gray-600 cursor-pointer md:p-4 dark:border-gray-500 dark:hover:bg-gray-900 hover:bg-slate-200 hover:font-bold'>
+                        {{ now()->create($year, $month, $day)->format('j') }}
+                    </div>
+                @endfor
+            </div>
+
         </div>
 
-        <div class="relative grid grid-cols-7 text-center">
-            @for ($i = 0; $i < $firstDayOfMonth; $i++)
-                <div wire:loading.remove wire:target="prevMonth, nextMonth, today"
-                    class='py-1 border border-gray-600 md:p-4 dark:border-white'></div>
-            @endfor
-            @for ($day = 1; $day <= $daysInMonth; $day++)
-                <div wire:loading.remove wire:target="prevMonth, nextMonth, today"
-                    class='py-1 border border-gray-600 md:p-4 dark:border-white'>
-                    {{ now()->create($year, $month, $day)->format('j') }}
-                </div>
-            @endfor
-
-
-        </div>
+        <h1>Current Full Date: </h1>
     </div>
 
 </div>
