c56207b00589d0a40d272b1edcb2e8e40f0b4ed3
JoshuaReuben <joshuareubenc.v1@gmail.com>
2024-07-04 02:09:36 +0800

    questions crud done

--- app/Livewire/Question/QuestionCreate.php
+++ app/Livewire/Question/QuestionCreate.php
@@ -7,7 +7,7 @@ use App\Models\Question;
 use App\Models\Lesson;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\On;
-use Illuminate\Validation\ValidationException;
+
 
 #[Layout('layouts.resource')]
 class QuestionCreate extends Component
@@ -23,6 +23,15 @@ class QuestionCreate extends Component
         ['choice' => ''],
         ['choice' => ''],
     ];
+
+    // For Determing the Lesson Position
+    public $passed_lesson;
+    public $fetched_lessons;
+    public $lesson_position;
+
+    // For Displaying the Created Quesitions
+    public $fetched_questions;
+
 
     protected $rules = [
         'question_asked' => 'required|min:5|max:500',
@@ -42,37 +51,22 @@ class QuestionCreate extends Component
         'choices.*.choice.max' => 'Choice may not be greater than 255 characters',
     ];
 
+    //////////////////////////////////////////////////////////////
+    // Variables for the Edit Mode of Question
+    public $question_asked_Mode_Edit = [];
+    public $question_asked_Mode_Edit_Copy = [];
+
+    public $correct_answer_Mode_Edit = [];
+    public $correct_answer_Mode_Edit_Copy = [];
+
+    public $choices_Mode_Edit = [];
+
+    public $choices_Mode_Edit_Copy = [];
 
 
 
-    public function hasAtLeastTwoChoices()
-    {
-        $nonEmptyChoices = array_filter($this->choices, fn ($choice) => !empty($choice['choice']));
-        return count($nonEmptyChoices) >= 2;
-    }
 
-
-
-    public function addChoice()
-    {
-        $this->choices[] = ['choice' => ''];
-    }
-
-    public function removeChoice($index)
-    {
-        array_splice($this->choices, $index, 1);
-    }
-
-
-
-    // For Determing the Lesson Position
-    public $passed_lesson;
-    public $fetched_lessons;
-    public $lesson_position;
-
-    // For Displaying the Created Quesitions
-    public $fetched_questions;
-
+    /////////////////////////////////////////////////////////////////
     public function mount($lessonID)
     {
         // Create
@@ -88,11 +82,126 @@ class QuestionCreate extends Component
         // Display Questions 
 
         $this->fetched_questions = $this->passed_lesson->questions()->get();
+        //////////
+
+        foreach ($this->fetched_questions as $question) {
+            $this->question_asked_Mode_Edit[$question->id] = $question->question;
+            $this->correct_answer_Mode_Edit[$question->id] = $question->correct_answer;
+            $this->choices_Mode_Edit[$question->id] = $question->choices;
+        }
+
+
+
+        //Backup Variables
+        $this->question_asked_Mode_Edit_Copy = $this->question_asked_Mode_Edit;
+        $this->correct_answer_Mode_Edit_Copy = $this->correct_answer_Mode_Edit;
+        $this->choices_Mode_Edit_Copy = $this->choices_Mode_Edit;
+
+        // dump($this->correct_answer_Mode_Edit[29]);
+        // dump($this->choices);
+        // dump($this->choices_Mode_Edit);
+        // dump($this->choices_Mode_Edit[26]);
     }
+
+
+    #[On('question-added')]
+    public function remountingVars()
+    {
+        $this->fetched_questions = $this->passed_lesson->questions()->get();
+        //////////
+
+        foreach ($this->fetched_questions as $question) {
+            $this->question_asked_Mode_Edit[$question->id] = $question->question;
+            $this->correct_answer_Mode_Edit[$question->id] = $question->correct_answer;
+            $this->choices_Mode_Edit[$question->id] = $question->choices;
+        }
+
+
+
+        //Backup Variables
+        $this->question_asked_Mode_Edit_Copy = $this->question_asked_Mode_Edit;
+        $this->correct_answer_Mode_Edit_Copy = $this->correct_answer_Mode_Edit;
+        $this->choices_Mode_Edit_Copy = $this->choices_Mode_Edit;
+    }
+
+
+
+    public function hasAtLeastTwoChoices()
+    {
+        $nonEmptyChoices = array_filter($this->choices, fn ($choice) => !empty($choice['choice']));
+        return count($nonEmptyChoices) >= 2;
+    }
+
+    public function hasAtLeastTwoChoices_Mode_Edit($questionID)
+    {
+        $nonEmptyChoices = array_filter($this->choices_Mode_Edit[$questionID], fn ($choice) => !empty($choice['choice']));
+        return count($nonEmptyChoices) >= 2;
+    }
+
+
+
+    public function addChoice()
+    {
+        $this->choices[] = ['choice' => ''];
+    }
+
+    public function addChoice_Edit_Mode($questionID)
+    {
+        $this->choices_Mode_Edit[$questionID][] = ['choice' => ''];
+        // dump($this->choices_Mode_Edit[26]);
+
+    }
+
+    public function removeChoice($index)
+    {
+        array_splice($this->choices, $index, 1);
+    }
+
+    public function removeChoice_Edit_Mode($questionID, $index)
+    {
+        array_splice($this->choices_Mode_Edit[$questionID], $index, 1);
+    }
+
+    public function cancelEditQuestion()
+    {
+        // Reset The Variables Only
+        $this->question_asked_Mode_Edit = $this->question_asked_Mode_Edit_Copy;
+        $this->correct_answer_Mode_Edit = $this->correct_answer_Mode_Edit_Copy;
+        $this->choices_Mode_Edit = $this->choices_Mode_Edit_Copy;
+    }
+
+
+
+
+
 
     public function updated($propertyName)
     {
         $this->validateOnly($propertyName);
+    }
+
+
+    public function lookForDuplicateChoices()
+    {
+        // Extract the choices into a simple array
+        $choiceValues = array_map(function ($item) {
+            return $item['choice'];
+        }, $this->choices);
+
+        // Count the occurrences of each choice
+        $choiceCounts = array_count_values($choiceValues);
+
+        // Find the duplicates
+        $hasDuplicates = false;
+        foreach ($choiceCounts as $count) {
+            if ($count > 1) {
+                $hasDuplicates = true;
+                break;
+            }
+        }
+
+        // Return whether there are duplicates
+        return $hasDuplicates;
     }
 
 
@@ -101,6 +210,13 @@ class QuestionCreate extends Component
     {
         $this->validate();
 
+        $hasDuplicate = $this->lookForDuplicateChoices();
+
+        if ($hasDuplicate) {
+            // Add an error message to the component
+            $this->addError('choices', 'Each choice must be unique. Duplicate choices are not allowed.');
+            return;
+        }
 
 
 
@@ -111,16 +227,44 @@ class QuestionCreate extends Component
             'correct_answer' => $this->correct_answer,
         ]);
 
-        session()->flash('message', 'Question added successfully!');
-        return redirect()->route('pages.admin.question', ['courseID' => $this->courseID, 'moduleID' => $this->moduleID, 'lessonID' => $this->lessonID]);
+        $this->reset(['question_asked', 'correct_answer', 'choices']);
+
+        // session()->flash('message', 'Question added successfully!');
+        // return redirect()->route('pages.admin.question', ['courseID' => $this->courseID, 'moduleID' => $this->moduleID, 'lessonID' => $this->lessonID]);
+        $this->dispatch('question-added');
     }
 
     public function deleteQuestion($questionID)
     {
         $question = Question::find($questionID);
         $question->delete();
+
         $this->dispatch('question-deleted');
     }
+
+    public function updateAQuestion($questionID)
+    {
+        $question = Question::find($questionID);
+        $question->update([
+            'question' => $this->question_asked_Mode_Edit[$questionID],
+            'choices' => $this->choices_Mode_Edit[$questionID],
+            'correct_answer' => $this->correct_answer_Mode_Edit[$questionID],
+        ]);
+        $this->dispatch('question-updated');
+    }
+
+
+    #[On('question-updated')]
+    #[On('question-added')]
+    public function refreshQuestions()
+    {
+        $this->fetched_questions = $this->passed_lesson->questions()->get();
+    }
+
+
+
+
+
 
     #[On('question-deleted')]
     public function render()
---
+++ resources/views/components/sweet-alert-2.blade.php
@@ -0,0 +1,24 @@
+@props(['on', 'color' => 'green', 'title' => 'Success', 'message' => 'Saved.'])
+
+<div x-data="{ shown: false, timeout: null }" x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout);
+    shown = true;
+    timeout = setTimeout(() => { shown = false }, 2000); })" x-transition:enter="transform transition-transform duration-300"
+    x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0"
+    x-transition:leave="transform transition-transform duration-300" x-transition:leave-start="translate-x-0"
+    x-transition:leave-end="translate-x-full" x-show="shown" @click="shown = false"
+    {{ $attributes->merge(['class' => 'fixed z-50 bottom-10 right-8 w-[300px]']) }}>
+    {{--  Content --}}
+    <div class=" w=[300px] flex items-center p-4 mb-4 text-sm text-{{ $color }}-800 border border-{{ $color }}-300 rounded-lg bg-{{ $color }}-50 cursor-pointer"
+        role="alert">
+        <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor" viewBox="0 0 20 20">
+            <path
+                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+
+        <div>
+            <span class="font-medium">{{ $title }}!</span> {{ $message }}
+        </div>
+    </div>
+    {{-- End Content --}}
+</div>
