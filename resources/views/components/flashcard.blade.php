390b86eb0f35a883ab0125dc6220cf132bc8c1b5
JoshuaReuben <joshuareubenc.v1@gmail.com>
2024-07-10 01:41:08 +0800

    carousel and flashcard

--- resources/views/components/flashcard.blade.php
+++ resources/views/components/flashcard.blade.php
@@ -1,3 +1,6 @@
+@props(['question' => '', 'answer' => ''])
+
+
 <div class="container mx-auto">
 
     {{--  Flashcard --}}
@@ -5,59 +8,42 @@
         class="[perspective:800px] mx-auto h-full w-11/12  cursor-pointer flex justify-center items-center">
 
         <div class="[transform-style:preserve-3d]  relative transition-transform duration-1000 "
-            :class="{ '[transform:rotateY(180deg)]': flipped }">
+            :class="{ '[transform:rotateX(180deg)]': flipped }">
 
             {{-- Add Absoulute after --}}
             <div class="[backface-visibility:hidden] w-full h-full absolute">
                 {{-- Start of Front --}}
 
                 <div
-                    class="flex justify-center flex-col items-center w-full md:min-w-[420px] md:max-w-[800px] md:w-11/12 min-h-[300px]  mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    class="flex justify-center flex-col items-center w-full  md:min-w-[420px] md:max-w-[1100px] min-h-[300px] md:min-h-[500px]  mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 ">
 
-                    <div class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-                        Noteworthy
-                        technology acquisitions 2021</div>
+                    <div class="mb-2 text-2xl font-bold tracking-tight text-center text-gray-900 dark:text-white">
+                        Question
+                    </div>
                     <div
-                        class="flex-grow flex items-center font-normal text-gray-700 dark:text-gray-400 h-full text-center">
-                        Here are the
-                        biggest
-                        enterprise
-                        technology
-                        acquisitions of 2021 so far, in reverse chronological order. Here are the biggest enterprise
-                        technology
-                        acquisitions of 2021 so far, in reverse chronological order Here are the biggest enterprise
-                        technology
-                        acquisitions of 2021 so far, in reverse chronological order. Here are the biggest enterprise
-                        technology
-                        acquisitions of 2021 so far, in reverse chronological order</div>
+                        class="flex items-center flex-grow h-full font-normal text-center text-gray-700 dark:text-gray-400">
+                        {{ $question }}
+                    </div>
                 </div>
 
                 {{-- End of Front --}}
             </div>
 
-            <div class="[backface-visibility:hidden] [transform:rotateY(180deg)] w-full h-full">
+            <div class="[backface-visibility:hidden] [transform:rotateX(180deg)] w-full h-full">
                 {{-- Start of Back --}}
 
 
                 <div
-                    class="flex justify-center flex-col items-center w-full md:min-w-[420px] md:max-w-[800px] md:w-11/12 min-h-[300px]  mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    class="flex justify-center flex-col items-center w-full  md:min-w-[420px] md:max-w-[1100px] min-h-[300px] md:min-h-[500px]  mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
 
-                    <div class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-                        Noteworthy
-                        technology acquisitions 2021</div>
+                    <div class="mb-2 text-2xl font-bold tracking-tight text-center text-gray-900 dark:text-white">
+                        Answer
+                    </div>
                     <div
-                        class="flex-grow flex items-center font-normal text-gray-700 dark:text-gray-400 h-full text-center">
-                        Here are the
-                        biggest
-                        enterprise
-                        technology
-                        acquisitions of 2021 so far, in reverse chronological order. Here are the biggest enterprise
-                        technology
-                        acquisitions of 2021 so far, in reverse chronological order Here are the biggest enterprise
-                        technology
-                        acquisitions of 2021 so far, in reverse chronological order. Here are the biggest enterprise
-                        technology
-                        acquisitions of 2021 so far, in reverse chronological order</div>
+                        class="flex items-center flex-grow h-full font-normal text-center text-gray-700 dark:text-gray-400">
+
+                        {{ $answer }}
+                    </div>
                 </div>
 
 
